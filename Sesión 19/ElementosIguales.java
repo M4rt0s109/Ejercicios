4017d91cef652dcf47e8b7c1084d2e1cf6eaea17
Diego pc casa <diegokyra2006@gmail.com>
2024-11-25 21:13:52 +0100

    ejercicio 4 sesion 19

---
+++ Sesión 19/ElementosIguales.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ElementosIguales {
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        System.out.println("Introduce el número de filas: ");
+        int filas = t.nextInt();
+        System.out.println("Introduce el número de columnas: ");
+        int columnas = t.nextInt();
+        int [ ] [ ] m = new int [filas] [columnas];
+        System.out.println("Introduce los componentes: ");
+        for(int i = 0; i < m.length; i++){
+            for (int j = 0; j < m[i].length; j++){
+                m[i] [j] = t.nextInt();
+            } 
+        }
+        System.out.println("Introduce la posición a comprobar: ");
+        int fil = t.nextInt();
+        int col = t.nextInt();
+        int elemento = m[fil][col];
+        System.out.println("El elemento a comparar es: " + elemento);
+        int contador_filas = 0;
+        for (int j = 0;j < columnas;j++){
+            if (m[fil][j] == elemento) contador_filas += 1;
+        }
+        int contador_columnas = 0;
+        for (int i = 0;i < filas;i++){
+            if (m[i][col] == elemento) contador_columnas+= 1;
+        }
+        System.out.println("El elemento se repite " + (contador_filas - 1) + " veces en la fila " + (fil));
+        System.out.println("El elemento se repite " + (contador_columnas - 1) + " veces en la columna " + (col));
+        t.close();
+    }
+}
--- Sesión 19/LeerMatrices.java
+++ Sesión 19/LeerMatrices.java
@@ -13,16 +13,26 @@ public class LeerMatrices {
         return m;
     }
     public static void ImprimirMatrices (int [ ] [ ] m) {
-        for (int i = 0; i < m.length; i++)
-            for (int j = 0; j < m[i].length;j++)
-                System.out.print(m[i] [j]);
+        for (int i = 0; i < m.length; i++){
+            for (int j = 0; j < m[i].length;j++){
+                System.out.print(m[i] [j] + " ");
+            }    
             System.out.println();
+        }
+    }
+    public static void ImprimeMatrizAlReves (int [ ] [ ] m){
+        for (int i = m.length - 1; i >= 0; i--){
+            for (int j = m[i].length - 1; j >= 0; j--){
+                System.out.print(m[i] [j] + " ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int [ ] [ ] matriz = LeerMatrices(teclado);
-        System.out.println("La matriz es: ");
-        ImprimirMatrices(matriz);
+        System.out.println("La matriz al revés es: ");
+        ImprimeMatrizAlReves(matriz);
         teclado.close();
     }
 }
